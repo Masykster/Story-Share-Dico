048575e7c3e2bae1e845427f45b51dbda28c232d
Abdullah Masykur <masykur6060@gmail.com>
2026-03-28 20:47:46 +0700

    Netify Setup

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // Gunakan '/' untuk root domain di Netlify
+  base: '/', 
+  
+  // Menentukan folder 'src' sebagai root pengembangan
   root: resolve(__dirname, 'src'),
+  
+  // Menentukan folder aset publik
   publicDir: resolve(__dirname, 'src', 'public'),
+  
   build: {
+    // Output hasil build akan berada di folder 'dist' di root proyek
     outDir: resolve(__dirname, 'dist'),
     emptyOutDir: true,
   },
+  
   resolve: {
     alias: {
+      // Memungkinkan penggunaan '@' untuk merujuk ke folder 'src'
       '@': resolve(__dirname, 'src'),
     },
   },
